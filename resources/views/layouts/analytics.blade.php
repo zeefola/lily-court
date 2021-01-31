89697d143f35ba224c1dfe0590f3dfecd08dbcb0
zeefola <zeefola@gmail.com>
2021-01-31 22:49:34 +0100

    Added google analytics

---
+++ resources/views/layouts/analytics.blade.php
@@ -0,0 +1,13 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-S3S66G0T8M"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-S3S66G0T8M');
+
+</script>
