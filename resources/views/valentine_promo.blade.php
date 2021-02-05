41b22c58163df6519846013d7d0bb88fe3f6b9bf
zeefola <zeefola@gmail.com>
2021-02-05 15:07:29 +0100

    Content modification

--- resources/views/valentine_promo.blade.php
+++ resources/views/valentine_promo.blade.php
@@ -68,7 +68,8 @@
                                         <span>Toilet</span>
                                     </li>
                                 </ul>
-                                <h1>30% special valentine offer on our apartments</h1>
+                                <h1>Win an all expense paid dinner this season in Lily Court valentine promo and lot
+                                    more prices</h1>
                                 <p>Your search for comfort and asset security is over. Get a 30% discount on our luxury
                                     and well detailed 2-3 bedroom apartment. You stand a chance to win an all-expense
                                     paid night at Radisson Blu Hotel when you pay instantly.</p>
