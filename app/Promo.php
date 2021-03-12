dfec455fa027bdea16dfa41248e3d363a2878696
Promise Ezema <promiseezema11@gmail.com>
2021-03-12 14:33:09 +0100

    Ready

--- app/Promo.php
+++ app/Promo.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Valentine extends Model
+class Promo extends Model
 {
     //
 }
--- database/migrations/2021_03_12_142942_create_promos_table.php
+++ database/migrations/2021_03_12_142942_create_promos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateValentinesTable extends Migration
+class CreatePromosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateValentinesTable extends Migration
      */
     public function up()
     {
-        Schema::create('valentines', function (Blueprint $table) {
+        Schema::create('promos', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
             $table->string('email')->nullable();
@@ -30,6 +30,6 @@ class CreateValentinesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('valentines');
+        Schema::dropIfExists('promos');
     }
 }
